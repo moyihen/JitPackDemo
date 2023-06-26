65579896460802974c03bd5646e8302fc4a22d17
moyihen <843369849@qq.com>
2023-06-26 18:01:39 +0800

    5

--- app/src/main/java/com/example/jitpackdemo/MainActivity.java
+++ app/src/main/java/com/example/jitpackdemo/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.jittoast.JToast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        JToast.show(this,"123456");
     }
 }
